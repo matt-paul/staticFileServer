8a85113278d971286954b8155d98f16d76db6cb5
Matt Paul <mattpaul001@icloud.com>
2016-01-03 21:51:10 +0000

    Refactored to use stream#pipe()

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
+//By using stream#pipe, the original server code can be greatly simplified
 var http = require('http');
 var parse = require('url').parse
 var join = require('path').join;
 var root = __dirname;
 
+//By using stream#pipe, the original server code can be greatly simplified
 var server = http.createServer(function(req, res) {
   var url = parse(req.url);
   var path = join(root, url.pathname);
   //construct absolute path
   var stream = fs.createReadStream(path); //create fs.ReadStream
-  stream.on('data', function(chunk) {
-    res.write(chunk);
-  });
-  stream.on('end', function() {
-    res.end();
-  });
+  stream.pipe(res);
 });
 
 server.listen(3000);
+//original version
+//var server = http.createServer(function(req, res) {
+//  var url = parse(req.url);
+//  var path = join(root, url.pathname);
+//  //construct absolute path
+//  var stream = fs.createReadStream(path); //create fs.ReadStream
+//  stream.on('data', function(chunk) {
+//    res.write(chunk);
+//  });
+//  stream.on('end', function() {
+//    res.end();
+//  });
+//});
